99d5d1f04dea0a5a8da54d514c478b61874c90aa
Sebastian <sgracianor@unal.edu.co>
2022-03-28 23:02:40 -0500

    version final

--- src/main/java/datos/MonologoA.java
+++ src/main/java/datos/MonologoA.java
@@ -18,6 +18,7 @@ public class MonologoA { //clase que alberga el metodo juego() para inicializar 
 
         System.out.println(respuestaPregunta1.get(0));
         String cadena1 = respuestaPregunta1.get(0).toString();
+        System.out.println("Llevas acumulado 100 dolares!");
         if (cadena1.equals("Verdadero")) {
             CategoriaB pregunta2 = new CategoriaB();
             pregunta2.pregunta2();
@@ -27,9 +28,9 @@ public class MonologoA { //clase que alberga el metodo juego() para inicializar 
             respuesta2.setAns(sc.nextLine());
             List<RespuestasA> respuestaPregunta2 = respuesta2.seleccionarA(respuesta2.getAns());
             System.out.println(respuestaPregunta2.get(0));
-            System.out.println("Llevas acumulado 500 dolares!");
             String cadena2 = respuestaPregunta2.get(0).toString();
             if (cadena2.equals("Verdadero")) {
+                System.out.println("Llevas acumulado 500 dolares!");
                 CategoriaC pregunta3 = new CategoriaC();
                 pregunta3.pregunta3();
                 pregunta3.opciones3();
@@ -38,9 +39,9 @@ public class MonologoA { //clase que alberga el metodo juego() para inicializar 
                 respuesta3.setAns(sc.nextLine());
                 List<RespuestasA> respuestaPregunta3 = respuesta3.seleccionarA(respuesta3.getAns());
                 System.out.println(respuestaPregunta3.get(0));
-                System.out.println("Llevas acumulado 1000 dolares!");
                 String cadena3 = respuestaPregunta3.get(0).toString();
                 if (cadena3.equals("Verdadero")) {
+                    System.out.println("Llevas acumulado 1000 dolares!");
                     CategoriaD pregunta4 = new CategoriaD();
                     pregunta4.pregunta4();
                     pregunta4.opciones4();
@@ -49,9 +50,9 @@ public class MonologoA { //clase que alberga el metodo juego() para inicializar 
                     respuesta4.setAns(sc.nextLine());
                     List<RespuestasA> respuestaPregunta4 = respuesta4.seleccionarA(respuesta4.getAns());
                     System.out.println(respuestaPregunta4.get(0));
-                    System.out.println("Llevas acumulado 1500 dolares!");
                     String cadena4 = respuestaPregunta4.get(0).toString();
                     if (cadena4.equals("Verdadero")) {
+                        System.out.println("Llevas acumulado 1500 dolares!");
                         CategoriaE pregunta5 = new CategoriaE();
                         pregunta5.pregunta5();
                         pregunta5.opciones5();
@@ -62,14 +63,14 @@ public class MonologoA { //clase que alberga el metodo juego() para inicializar 
                         System.out.println(respuestaPregunta5.get(0));
                         System.out.println("Felicitaciones has ganado y te llevaste 2000 dolares!!");
                     } else {
-                        System.out.println("Perdiste y solo te llevas 1500 dolares");
+                        System.out.println("Perdiste y solo te llevas 1000 dolares");
                     }
 
                 } else {
-                    System.out.println("Perdiste y solo te llevas 1000 dolares");
+                    System.out.println("Perdiste y solo te llevas 500 dolares");
                 }
             } else {
-                System.out.println("Perdiste y solo te llevas 500 dolares");
+                System.out.println("Perdiste y solo te llevas 100 dolares");
             }
         } else {
             System.out.println("Perdiste");
